c4f7aec5dacabde792ddab2663a0d80716e128cb
DanielZehrfeld <daniel.zehrfeld@gmx.de>
2017-02-21 11:31:02 +0100

    Added comments

--- TinyBus/ITinyBusMessage.cs
+++ TinyBus/ITinyBusMessage.cs
@@ -2,6 +2,10 @@
 {
     public interface ITinyBusMessage<out T>
     {
+        /// <summary>
+        /// Is called to create a copy of the bradcasted message for each recipient
+        /// </summary>
+        /// <returns>A copy of the message</returns>
         T Clone();
     }
 }
--- TinyBus/TinyBusControl.cs
+++ TinyBus/TinyBusControl.cs
@@ -4,15 +4,31 @@ using System.Threading.Tasks;
 
 namespace TinyBus
 {
+    /// <summary>
+    /// Used to bradcast messages to subscribers and collect returned messages.
+    /// This class is thread safe
+    /// </summary>
     public interface ITinyBusControl
     {
+        /// <summary>
+        /// Register a message recipient
+        /// </summary>
+        /// <typeparam name="T">The type of message that the recipient shell be registered for</typeparam>
+        /// <param name="handler">The handler that is called when a message is broadcasted</param>
         void Handle<T>(Func<T, T> handler);
+
+        /// <summary>
+        /// Publish a message
+        /// </summary>
+        /// <typeparam name="T">The type of message to publish</typeparam>
+        /// <param name="message">The message to publish. Each recipients will get a copy of the message</param>
+        /// <returns>Collection of result messages returned by the recipients</returns>
         Task<T[]> Publish<T>(T message) where T : ITinyBusMessage<T>;
     }
 
     public class TinyBusControl : ITinyBusControl
     {
-        readonly TinyHandlers _handlers = new TinyHandlers();
+        private readonly TinyHandlers _handlers = new TinyHandlers();
 
         public void Handle<T>(Func<T, T> handler)
         {
